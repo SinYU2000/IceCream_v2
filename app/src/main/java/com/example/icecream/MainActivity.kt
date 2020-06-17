d0ae5a09ad211b19671e41395c62e070abcacf9f
SinYU2000 <57216981+SinYU2000@users.noreply.github.com>
2020-06-18 05:10:31 +0800

    interface

--- app/src/main/java/com/example/icecream/MainActivity.kt
+++ app/src/main/java/com/example/icecream/MainActivity.kt
@@ -1,12 +1,32 @@
  package com.example.icecream
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+
+ class MainActivity : AppCompatActivity() {
+
+
+     private var add_btn : Button? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        add_btn = findViewById(R.id.add)
+
+        add_btn?.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(view: View?) {
+                val intent = Intent()
+                intent.setClass(this@MainActivity, AddTask::class.java)
+                startActivity(intent);
+            }
+
+        })
+
     }
+
 }
